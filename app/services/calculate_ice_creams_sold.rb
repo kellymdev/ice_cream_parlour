c7a9eda1bf30b56790ddac8e53619d6e62cfeaf3
Kelly Munro <kelly.munro@fluxfederation.com>
2018-10-18 16:17:58 +1300

    Add a CalculateIceCreamsSold service

---
+++ app/services/calculate_ice_creams_sold.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class CalculateIceCreamsSold
+  attr_reader :ice_creams_made, :pedestrian_count, :price
+
+  def initialize(ice_creams_made, pedestrian_count, price)
+    @ice_creams_made = ice_creams_made
+    @pedestrian_count = pedestrian_count
+    @price = price
+  end
+
+  def call
+    calculate_ice_creams_sold
+  end
+
+  private
+
+  def calculate_ice_creams_sold
+    sold = (pedestrian_count * price_multiplier).to_i
+
+    if ice_creams_made > sold
+      sold
+    else
+      ice_creams_made
+    end
+  end
+
+  def price_multiplier
+    case price_in_cents
+    when 0..50
+      0.8
+    when 51..100
+      0.65
+    when 101..150
+      0.50
+    when 151..200
+      0.4
+    else
+      0.25
+    end
+  end
+
+  def price_in_cents
+    price * 100
+  end
+end
---
+++ spec/services/calculate_ice_creams_sold_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.describe CalculateIceCreamsSold, type: :service do
+  describe '#call' do
+    let(:ice_creams_made) { 20 }
+    let(:pedestrian_count) { 20 }
+    let(:price) { 1.0 }
+    subject(:service) { described_class.new(ice_creams_made, pedestrian_count, price) }
+
+    context 'when the number of ice creams sold is less than the number of ice creams made' do
+      let(:ice_creams_made) { 20 }
+
+      it 'returns the number of ice creams sold' do
+        expect(service.call).to eq 13
+      end
+    end
+
+    context 'when the number of ice creams sold is more than the number of ice creams made' do
+      let(:ice_creams_made) { 5 }
+
+      it 'returns the number of ice creams made' do
+        expect(service.call).to eq 5
+      end
+    end
+
+    context 'based on price' do
+      context 'when the price is between 0.00 and 0.50' do
+        let(:price) { 0.45 }
+
+        it 'uses a multiplier of 0.8' do
+          expect(service.call).to eq 16
+        end
+      end
+
+      context 'when the price is between 0.51 and 1.00' do
+        let(:price) { 0.75 }
+
+        it 'uses a multiplier of 0.65' do
+          expect(service.call).to eq 13
+        end
+      end
+
+      context 'when the price is between 1.01 and 1.50' do
+        let(:price) { 1.25 }
+
+        it 'uses a multiplier of 0.5' do
+          expect(service.call).to eq 10
+        end
+      end
+
+      context 'when the price is between 1.51 and 2.00' do
+        let(:price) { 1.75 }
+
+        it 'uses a multiplier of 0.4' do
+          expect(service.call).to eq 8
+        end
+      end
+
+      context 'when the price is more than 2.00' do
+        let(:price) { 2.25 }
+
+        it 'uses a multiplier of 0.25' do
+          expect(service.call).to eq 5
+        end
+      end
+    end
+  end
+end
