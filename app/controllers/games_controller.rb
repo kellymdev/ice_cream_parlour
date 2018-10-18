efff2365ec83abf7ea360ed0519bbe4816cfb2ac
Kelly Munro <kelly.munro@fluxfederation.com>
2018-10-19 08:24:24 +1300

    Add basic show action and view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 class GamesController < ApplicationController
+  before_action :find_game, only: [:show]
+
   def index
     @games = Game.all
   end
 
   def new
     @game = Game.new
+  end
+
+  def show
+  end
+
+  private
+
+  def find_game
+    @game = Game.find(params[:id])
   end
 end
