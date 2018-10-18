397f50799ac5941f2b59a41d91d0ec7b7a60f88f
Kelly Munro <kelly.munro@fluxfederation.com>
2018-10-18 16:41:47 +1300

    Add a new Game form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,4 +4,8 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def new
+    @game = Game.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'games#index'
 
-  resources :games, only: [:index, :show, :create]
+  resources :games, only: [:index, :show, :new, :create]
 end
