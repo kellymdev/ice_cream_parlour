066fb179d9c51ec92749d83a6e2e387077aaeab2
Kelly Munro <kelly.munro@fluxfederation.com>
2018-11-22 16:13:35 +1300

    Refactor the inventory display

--- features/create_game.feature
+++ features/create_game.feature
@@ -9,5 +9,6 @@ Scenario: A user can create a new game
    When I enter "Amazing Ice Creams" into the "Name" field
     And I click the "Create Game" button
    Then I should see "Amazing Ice Creams"
-    And I should see "Balance"
-    And I should see "$5.00"
+    And I should see a balance of "$5.00"
+    And I should see a milk inventory of "0"
+    And I should see a sugar inventory of "0"
--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -9,3 +9,21 @@ end
 Given("I am on the game details screen for the game") do
   visit(game_path(@game))
 end
+
+Then("I should see a balance of {string}") do |balance|
+  within('.current-balance') do
+    expect(page).to have_content(balance)
+  end
+end
+
+Then("I should see a milk inventory of {string}") do |count|
+  within('.milk-inventory') do
+    expect(page).to have_content(count)
+  end
+end
+
+Then("I should see a sugar inventory of {string}") do |count|
+  within('.sugar-inventory') do
+    expect(page).to have_content(count)
+  end
+end
